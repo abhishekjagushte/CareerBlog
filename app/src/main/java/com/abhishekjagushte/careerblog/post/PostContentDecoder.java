4290ad261de30e0c9b30125ef67d115851bb14d4
abhishekjagushte <abhishekjagushte@gmail.com>
2020-03-17 00:06:39 +0530

    POST PAGE ADDED

--- app/src/main/java/com/abhishekjagushte/careerblog/post/PostContentDecoder.java
+++ app/src/main/java/com/abhishekjagushte/careerblog/post/PostContentDecoder.java
@@ -74,7 +74,6 @@ public class PostContentDecoder {
                                 view.setMovementMethod(LinkMovementMethod.getInstance());
                             }
                         });
-                        return;
 
                     }
 
